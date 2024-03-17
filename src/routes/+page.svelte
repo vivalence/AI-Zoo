b8bb2f413a7dc6ccaff639980104e5ff0a24bf72
mateusmoov <mateusvasconcelos4210@gmail.com>
2024-03-17 20:28:43 -0300

    feat: add chart component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
 <script>
+	import Chart from '$lib/components/Chart/Chart.svelte';
 	import PriceCalculator from '$lib/components/PriceCalculator/PriceCalculator.svelte';
 </script>
 
 <main class="flex-grow container mx-auto">
-	<PriceCalculator />
+	<div class="flex justify-center mt-12">
+		<PriceCalculator />
+		<Chart />
+	</div>
 </main>
