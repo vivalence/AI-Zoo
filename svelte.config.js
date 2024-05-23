173d017de733ca8e9297386bd0815fdb30e75ac3
mateusmoov <mateusvasconcelos4210@gmail.com>
2024-05-23 19:01:12 -0300

    fix: redirects in root

--- svelte.config.js
+++ svelte.config.js
@@ -3,25 +3,20 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
-	kit: {
-		alias: {
-			$lib: './src/lib',
-			$data: './src/data'
-		},
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : '/AI-Zoo'
-		}
-	}
+  kit: {
+    alias: {
+      $lib: './src/lib',
+      $data: './src/data'
+    },
+    adapter: adapter({
+      fallback: '404.html'
+    }),
+    paths: {
+      base: process.argv.includes('dev') ? '' : '/AI-Zoo'
+    }
+  }
 };
 
 export default config;
